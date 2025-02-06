c4ffb2dfbfcd89349468324f8a74a317e57c5e08
aniket7075 <129082391+aniket7075@users.noreply.github.com>
2025-02-06 16:23:24 +0530

    Done

---
+++ src/main/java/com/autopart/AutopartApplication.java
@@ -0,0 +1,13 @@
+package com.autopart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopartApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/autopart/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.autopart;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutopartApplication.class);
+	}
+
+}
