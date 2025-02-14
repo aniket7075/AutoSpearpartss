4bd5be81b46285b67f2575a96a134a66ac08cef8
aniket7075 <129082391+aniket7075@users.noreply.github.com>
2025-02-14 22:51:14 +0530

    Update OrderRepository.java

--- src/main/java/com/autopart/respo/OrderRepository.java
+++ src/main/java/com/autopart/respo/OrderRepository.java
@@ -3,7 +3,8 @@ package com.autopart.respo;
 import com.autopart.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> 
+{
     // You can add custom queries if needed here
 }
 
